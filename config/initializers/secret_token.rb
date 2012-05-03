a8f67b712e669e42fd5237daf5f1e1996cdf307c
Nolan Blew <nolanblew@hotmail.com>
2012-05-02 19:04:05 -0700

    First commit - started project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BhsSms::Application.config.secret_token = 'feb8e87753346c1cf0ec03f2bae994e6b6e1ffaa89e1113ae79a93b6ab169e862c212788ef159e9893b611cf0b3885628515b121c8b099f212368d5156774c91'
